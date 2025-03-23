ebf4a8c09f0745855cf609347cc6450995200a21
Nguyen-Thanh-Huy-io <nguyenthanhhuy021005@gmail.com>
2025-03-23 22:24:07 +0700

    product function and route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.route.js';
+import productsRoutes from './routes/products.route.js';
+
 import { connectDB } from './lib/db.js';
 
 
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/products",productsRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
